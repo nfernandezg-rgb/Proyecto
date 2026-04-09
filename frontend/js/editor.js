352a52685b1e3ac7195b1b540bfe0de45b7a6b6a
NELSON GERARDO FERNANDEZ GONZALEZ <nfernandezg@ucenfotec.ac.cr>
2026-04-08 21:52:12 -0600

    feat: creacion del formulario para crear un evento

--- frontend/js/editor.js
+++ frontend/js/editor.js
@@ -37,3 +37,53 @@ document.addEventListener("click", (e) => {
     }
 
 });
+
+
+//formulario crear evento
+
+function nextStep() {
+    document.getElementById("step-1").style.display = "none";
+    document.getElementById("step-2").style.display = "block";
+
+    document.getElementById("step1-indicator").classList.remove("bg-primary");
+    document.getElementById("step1-indicator").classList.add("bg-secondary");
+
+    document.getElementById("step2-indicator").classList.remove("bg-secondary");
+    document.getElementById("step2-indicator").classList.add("bg-primary");
+}
+
+function prevStep() {
+    document.getElementById("step-1").style.display = "block";
+    document.getElementById("step-2").style.display = "none";
+
+    document.getElementById("step1-indicator").classList.add("bg-primary");
+    document.getElementById("step1-indicator").classList.remove("bg-secondary");
+
+    document.getElementById("step2-indicator").classList.add("bg-secondary");
+    document.getElementById("step2-indicator").classList.remove("bg-primary");
+}
+
+
+//captura de datos
+
+document.getElementById("form-evento").addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const evento = {
+        nombre: document.getElementById("nombre").value,
+        fecha: document.getElementById("fecha").value,
+        horaInicio: document.getElementById("horaInicio").value,
+        horaFin: document.getElementById("horaFin").value,
+        descripcion: document.getElementById("descripcion").value,
+        objetivos: document.getElementById("objetivos").value,
+        agenda: document.getElementById("agenda").value,
+        agendaFacil: document.getElementById("agendaFacil").value,
+        publico: document.getElementById("publico").value,
+        infoAdicional: document.getElementById("infoAdicional").value,
+        estado: "pendiente"
+    };
+
+    console.log("Evento listo para backend:", evento);
+
+    alert("Evento enviado para aprobación :white_check_mark:");
+});
