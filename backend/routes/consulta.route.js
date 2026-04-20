8cb1f03ab2cdcc959c163172491b7a06aa20b1cd
NELSON GERARDO FERNANDEZ GONZALEZ <nfernandezg@ucenfotec.ac.cr>
2026-04-19 22:10:15 -0600

    fix: se ajusto la ruta de consultas para poder asociar correctamente a un evento y ahora pasar a mostrar en el dashboard de editor

--- backend/routes/consulta.route.js
+++ backend/routes/consulta.route.js
@@ -1,3 +1,5 @@
+const Evento = require("../models/evento.model.js");
+
 const express = require("express");
 const router = express.Router();
 const Consulta = require("../models/consulta.model");
@@ -15,8 +17,35 @@ router.post("/", async (req, res) => {
 
 // obtener consultas
 router.get("/", async (req, res) => {
-    const consultas = await Consulta.find();
-    res.json(consultas);
+    try {
+
+        const consultas = await Consulta.find();
+
+        const resultado = [];
+
+        for (let c of consultas) {
+
+            const evento = await Evento.findById(c.eventoId);
+
+            resultado.push({
+                _id: c._id,
+                correo: c.correo,
+                mensaje: c.mensaje,
+                fecha: c.createdAt || new Date(),
+
+                evento: evento ? {
+                    nombre: evento.nombre,
+                    fecha: evento.fecha
+                } : null
+            });
+        }
+
+        res.json(resultado);
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Error obteniendo consultas" });
+    }
 });
 
 module.exports = router;
